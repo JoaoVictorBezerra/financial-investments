4dc281afd694b75d9ac20de30cd0c7d98d455afd
João Victor Bezerra da Silva <dev.joaovictor@hotmail.com>
2024-04-11 20:42:48 +0100

    initial config

---
+++ src/main/java/tech/projects/financialinvestments/FinancialInvestmentsApplication.java
@@ -0,0 +1,13 @@
+package tech.projects.financialinvestments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialInvestmentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialInvestmentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/projects/financialinvestments/FinancialInvestmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.projects.financialinvestments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialInvestmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
